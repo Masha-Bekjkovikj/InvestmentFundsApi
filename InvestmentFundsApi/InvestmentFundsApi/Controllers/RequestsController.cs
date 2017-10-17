984ad7011344b121fb44fe6c9278cd6282341c4f
Masha Bekjkovikj <Masha.Bekjkovikj@asseco-see.mk>
2017-10-17 14:24:14 +0200

    RequestsController and SalesController added

---
+++ InvestmentFundsApi/InvestmentFundsApi/Controllers/RequestsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace InvestmentFundsApi.Controllers
+{
+    public class RequestsController : ApiController
+    {
+        [HttpPost]
+        public void Post(int FundId, int SellType, Decimal Amount, int SellBank, string SellAccount)
+        {
+        }
+    }
