013639ebd96ad4beb76743001195aea898e4a2c0
Masha Bekjkovikj <Masha.Bekjkovikj@asseco-see.mk>
2017-10-18 10:15:04 +0200

    Fund -> added model and controller

---
+++ InvestmentFundsApi/InvestmentFundsApi/Controllers/FundsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using InvestmentFundsApi.Models;
+
+namespace InvestmentFundsApi.Controllers
+{
+    public class FundsController : ApiController
+    {
+        // GET: api/Funds
+        public List<Fund> Get()
+        {
+            return new List<Fund>();
+        }
+
+        // GET: api/Funds/5
+        public Fund Get(int id)
+        {
+            return new Fund();
+        }
+
+        
+    }
+}
---
+++ InvestmentFundsApi/InvestmentFundsApi/Models/Fund.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvestmentFundsApi.Models
+{
+    public class Fund
+    {
+        public int fundId { get; set; }
+
+        public String fundName { get; set; }
+
+        public Decimal fundShareNumber { get; set; }
+
+        public Decimal fundSharePrice { get; set; }
+
+        public Decimal fundState { get; set; }
+
+        public DateTime fundDateCalculated { get; set; }
+
+
+    }
+}
