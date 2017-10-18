9a3afde55b2bdcdb13260ab3c444281d023c3b20
Masha Bekjkovikj <Masha.Bekjkovikj@asseco-see.mk>
2017-10-18 11:29:32 +0200

    TransactionsController added

---
+++ InvestmentFundsApi/InvestmentFundsApi/Controllers/TransactionsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using InvestmentFundsApi.Models;
+
+namespace InvestmentFundsApi.Controllers
+{
+    public class TransactionsController: ApiController
+    {
+        // GET: api/Funds
+        public List<Transaction> Get()
+        {
+            return new List<Transaction>();
+        }
+
+        
+
+        
+    }
+}
